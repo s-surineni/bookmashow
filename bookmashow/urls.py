6b068ce83066476a2198a72637c990ee9fcbdbb5
Sampath Surineni <sampath.skt@gmail.com>
2020-09-13 16:57:06 +0530

    removed redundant code

--- bookmashow/urls.py
+++ bookmashow/urls.py
@@ -16,16 +16,9 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 
-from ticketing import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
 
 urlpatterns = [
     path('', include('ticketing.urls')),
-    path('router/', include(router.urls)),
     # path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework'))
